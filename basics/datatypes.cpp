be49451fb73e312301493cbbdda042eeeb3c7eb4
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-04-13 23:44:42 +0530

    Added Cpp program for basic datatypes including string

---
+++ basics/datatypes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Integer (whole number)
+    int a = 10;
+
+    // Float (decimal - less precision)
+    float b = 10.5;
+
+    // Double (decimal - more precision)
+    double c = 10.123456;
+
+    // Character (single letter)
+    char d = 'A';
+
+    // Boolean (true/false)
+    bool e = true;
+
+    // String (multiple characters / word / sentence)
+    string f = "Jaykishan";
+
+    // Output
+    cout << "Integer: " << a << endl;
+    cout << "Float: " << b << endl;
+    cout << "Double: " << c << endl;
+    cout << "Character: " << d << endl;
+    cout << "Boolean: " << e << endl;
+    cout << "String: " << f << endl;
+
+    return 0;
+}
