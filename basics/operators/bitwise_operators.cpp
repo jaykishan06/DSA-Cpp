9a68a8f7d4c79a128f994d9d4cf02f37cfb93a6c
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-04-15 00:06:23 +0530

    added basci bitwise operators in cpp

---
+++ basics/operators/bitwise_operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ int main (){
+	 cout << (4&5)<<endl;
+	 cout << (~5)<<endl;
+	cout <<(7<< 2)<<endl; //left shift mai multiply karte h 7 x 2 pow n 
+	cout <<(100>> 3)<<endl; // right shift main divide karte h 100 divide 2 pow n 
+	 cout << (7^7)<< endl; // Same bits → 0 Different bits → 1
+
+     int a = 5; int b = a++ + ++a; std::cout << b;
+	return 0;
+ }
