bba2ade64c2111292586b8f223a4f06b85f2c7ea
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-04-14 14:40:23 +0530

    added basic arithmetics operations program in cpp

---
+++ basics/operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ int main(){
+	int a,b;
+	cout<<"enter the value of a"<<endl;
+	cin>>a;
+	cout<<"enter the value of b"<<endl;
+	cin>>b;
+	
+	//arithmetics operators
+	cout<<"additon "<<a+b<<endl;
+	cout<<"subtraction "<<a-b<<endl;
+	cout<<"multiplication "<<a*b<<endl;
+	cout<<"division "<<a/b<<endl;
+	cout<<"mod"<<a%b<<endl;
+	cout<<"jackkkk"<<endl;
+	
+	return 0;
+ }
