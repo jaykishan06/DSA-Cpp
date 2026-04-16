3ece27bebe3eeea216c0727ef7a2bf2fffa52ec3
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-04-16 23:51:13 +0530

    added assisgnment operators

---
+++ basics/operators/assisgment_operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ using namespace std;
+  int main (){
+	int arr= 56;
+	cout << arr<<endl;
+	//  arr = arr +  20;
+	 arr += 20;
+	 cout<< arr<<endl;
+	 arr -=10;
+	  cout<< arr << endl;
+	arr *=20;
+	cout<< arr << endl;
+	
+	
+	
+	 return 0;
+  }
