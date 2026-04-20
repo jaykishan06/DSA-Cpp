70a2fc5846f597e3dc0e2f0632d6e6b9af010aef
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-04-20 23:30:24 +0530

    implemented while_lopp in cpp

---
+++ basics/loops/while_loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+// Jab tak condition true hai tab tak loop chalega
+int main() {
+
+    int i = 1;
+
+    while(i <= 10) {
+        cout << i << " ";
+        i++;
+    }
+
+    return 0;
+}
