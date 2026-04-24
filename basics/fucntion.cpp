43fc6d0425ac74040fb483314f198ef701d98ad3
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-04-24 23:56:36 +0530

    implemted basic function in cpp

---
+++ basics/fucntion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// function definition
+void greet() {
+    cout << "Hello, welcome to C++!" << endl;
+}
+
+int main() {
+    greet();   // function call
+    return 0;
+}
