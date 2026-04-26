52314845bdb59987d8b5d231616ffae2b854c3e6
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-04-26 14:56:16 +0530

    Solved read and print integer problem using basic input output

---
+++ practice/basic_program/read_and_print_integer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Problem: Read and Print an Integer
+// Concept: Basic Input/Output
+
+int main() {
+    int num;
+    cin >> num;        // input
+    cout << num << endl;  // output same number
+    return 0;
+}
