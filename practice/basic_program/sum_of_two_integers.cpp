eb54a1a80bb1b54728099c07cce47fd57f498d52
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-04-27 14:51:06 +0530

    Solved sum of two integers using basic addition

---
+++ practice/basic_program/sum_of_two_integers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Problem: Sum of Two Integers
+// Concept: Basic Input/Output + Addition
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b << endl;
+
+    return 0;
+}
