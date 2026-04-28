5097e34f2cd7fe8d1f0e845f87129bdb7a6f9948
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-04-28 23:42:24 +0530

    Solved area of rectangle using multiplication

---
+++ practice/basic_program/area_of_rectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long length, breadth;
+    cin >> length >> breadth;
+
+    long long area = length * breadth;
+    cout << area << endl;
+
+    return 0;
+}
