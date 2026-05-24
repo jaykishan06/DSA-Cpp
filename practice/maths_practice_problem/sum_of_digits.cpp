4ca68d35eb2362e029f04b30302b1a5dea7fe1d9
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-05-24 23:31:23 +0530

    Add sum of digits solution

---
+++ practice/maths_practice_problem/sum_of_digits.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int num) {
+
+    int sum = 0;
+
+    while(num > 0) {
+        sum += num % 10; // last digit add karo
+        num /= 10;       // last digit remove karo
+    }
+
+    return sum;
+}
+
+int main() {
+
+    int num;
+    cin >> num;
+
+    cout << sumOfDigits(num);
+
+    return 0;
+}
