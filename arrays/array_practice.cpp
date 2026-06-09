a987da18b427be2718932d769c5031ff43593b24
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-06-09 17:43:59 +0530

    arranged the file in systematic way

---
+++ arrays/array_practice.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+	//2d array 
+	//creation 
+	//decalaration
+	int arr[3][2]={
+	{2,3},
+	{9,0},
+	{2,0},
+	};
+	cout<<"all good"<<endl;
+	//access
+	cout<< arr[2][1]<<endl;
+	cout<<arr[1][0];
+	return 0;
+}
