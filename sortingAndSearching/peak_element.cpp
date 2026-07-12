4dfe00cf9d1c30b1608a7507ed63cd8dd4ecde9d
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-07-12 23:47:47 +0530

    Add binary search solution for find peak element

--- sortingAndSearching/peak_element.cpp
+++ sortingAndSearching/peak_element.cpp
@@ -2,37 +2,48 @@
 #include <vector>
 using namespace std;
 
-// Binary search approach - O(log n)
-int findPeakElement(vector<int>& nums) {
-    int left = 0, right = nums.size() - 1;
+int findPeakElement(vector<int>& nums)
+{
+    int start = 0;
+    int end = nums.size() - 1;
 
-    while (left < right) {
-        int mid = left + (right - left) / 2;
+    while(start < end)
+    {
+        int mid = start + (end - start) / 2;
 
-        if (nums[mid] < nums[mid + 1]) {
-            // Upward slope -> peak lies on the right side
-            left = mid + 1;
-        } else {
-            // Downward slope -> peak lies on the left side (mid included)
-            right = mid;
+        if(nums[mid] < nums[mid + 1])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid;
         }
     }
 
-    return left; // left == right, pointing to a peak
+    return start;
 }
 
-int main() {
-    vector<int> test1 = {1, 2, 3, 1};
-    vector<int> test2 = {1, 2, 1, 3, 5, 6, 4};
-    vector<int> test3 = {1};
-    vector<int> test4 = {1, 2};
-    vector<int> test5 = {2, 1};
+int main()
+{
+    int n;
 
-    cout << "Test 1: index " << findPeakElement(test1) << " (expected 2)\n";
-    cout << "Test 2: index " << findPeakElement(test2) << " (expected 1 or 5)\n";
-    cout << "Test 3: index " << findPeakElement(test3) << " (expected 0)\n";
-    cout << "Test 4: index " << findPeakElement(test4) << " (expected 1)\n";
-    cout << "Test 5: index " << findPeakElement(test5) << " (expected 0)\n";
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter array elements: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    int index = findPeakElement(nums);
+
+    cout << "Peak Element Index: " << index << endl;
+    cout << "Peak Element Value: " << nums[index];
 
     return 0;
 }
