4b070d09f5d5d61d4f87419f7e2075e0c1a1c567
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-07-19 23:51:29 +0530

    Add binary search solution for finding square root

---
+++ sortingAndSearching/square_root_binary_search.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int mySqrt(int x)
+{
+    int start = 0;
+    int end = x;
+    int ans = 0;
+
+    while(start <= end)
+    {
+        long long mid = start + (end - start) / 2;
+
+        if(mid * mid == x)
+        {
+            return mid;
+        }
+        else if(mid * mid < x)
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int x;
+
+    cout << "Enter a number: ";
+    cin >> x;
+
+    cout << "Square Root (Integer Part): " << mySqrt(x);
+
+    return 0;
+}
