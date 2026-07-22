3a73bd62e7dd372467bcf96281768b755d156c24
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-07-22 23:51:01 +0530

    logic for quotient binary search

--- sortingAndSearching/find_quotient_binary_search.cpp
+++ sortingAndSearching/find_quotient_binary_search.cpp
@@ -1,61 +1,47 @@
-#include <iostream>
-#include <climits>
-#include <cmath>
-using namespace std;
+// class Solution {
+// public:
+//     long long int findQuotient(int dividend, int divisor)
+//     {
+//         // Division by zero
+//         if(divisor == 0)
+//         {
+//             return INT_MAX;
+//         }
 
-long long findQuotient(int dividend, int divisor)
-{
-    if(divisor == 0)
-    {
-        return INT_MAX;
-    }
+//         // Work with positive values
+//         long long a = abs((long long)dividend);
+//         long long b = abs((long long)divisor);
 
-    long long a = abs((long long)dividend);
-    long long b = abs((long long)divisor);
+//         long long start = 0;
+//         long long end = a;
+//         long long ans = 0;
 
-    long long start = 0;
-    long long end = a;
-    long long ans = 0;
+//         while(start <= end)
+//         {
+//             long long mid = start + (end - start) / 2;
 
-    while(start <= end)
-    {
-        long long mid = start + (end - start) / 2;
+//             if(mid * b == a)
+//             {
+//                 ans = mid;
+//                 break;
+//             }
+//             else if(mid * b < a)
+//             {
+//                 ans = mid;
+//                 start = mid + 1;
+//             }
+//             else
+//             {
+//                 end = mid - 1;
+//             }
+//         }
 
-        if(mid * b == a)
-        {
-            ans = mid;
-            break;
-        }
-        else if(mid * b < a)
-        {
-            ans = mid;
-            start = mid + 1;
-        }
-        else
-        {
-            end = mid - 1;
-        }
-    }
+//         // Handle sign
+//         if((dividend < 0) ^ (divisor < 0))
+//         {
+//             return -ans;
+//         }
 
-    if((dividend < 0) ^ (divisor < 0))
-    {
-        return -ans;
-    }
-
-    return ans;
-}
-
-int main()
-{
-    int dividend, divisor;
-
-    cout << "Enter Dividend: ";
-    cin >> dividend;
-
-    cout << "Enter Divisor: ";
-    cin >> divisor;
-
-    cout << "Quotient = " << findQuotient(dividend, divisor);
-
-    return 0;
-}
+//         return ans;
+//     }
+// };
