c3741b5e56f62c047ad9d4843391fdf44499e56a
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-08-04 19:16:09 +0530

    Add two pointer solution for reverse vowels of string

---
+++ string/reverse_vowels_of_string.cpp
@@ -0,0 +1,37 @@
+// class Solution {
+// public:
+
+//     bool isVowel(char ch)
+//     {
+//         ch = tolower(ch);
+
+//         return ch == 'a' || ch == 'e' || ch == 'i' ||
+//                ch == 'o' || ch == 'u';
+//     }
+
+//     string reverseVowels(string s)
+//     {
+//         int i = 0;
+//         int j = s.length() - 1;
+
+//         while(i < j)
+//         {
+//             if(!isVowel(s[i]))
+//             {
+//                 i++;
+//             }
+//             else if(!isVowel(s[j]))
+//             {
+//                 j--;
+//             }
+//             else
+//             {
+//                 swap(s[i], s[j]);
+//                 i++;
+//                 j--;
+//             }
+//         }
+
+//         return s;
+//     }
+// };
