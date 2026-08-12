30588f2b81df87ec2d15b6ce535e8456f057486f
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-08-12 23:18:43 +0530

    added logic for kth smallest element

---
+++ arrays/kth_Smallest_element.cpp
@@ -0,0 +1,11 @@
+// class solution
+// {
+// 	public:
+// 	int findTheSmallestElement(vector<int>& arr, int k){
+// 		vector<int> temp = arr;
+// 		sort(temp.begin(),temp.end());
+// 		return temp[k-1];
+		
+		
+// 	}
+// }
