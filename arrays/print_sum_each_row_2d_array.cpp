8f1a5b9b44ca6c87e4a178f7b4d73f487b784ac5
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-08-14 13:10:16 +0530

    added logic for solving print_sum_each_row_2d_array

---
+++ arrays/print_sum_each_row_2d_array.cpp
@@ -0,0 +1,17 @@
+// vector<int> rowSums(const vector<vector<int>>& arr) {
+
+//     vector<int> ans;
+
+//     for (int i = 0; i < arr.size(); i++) {
+
+//         int sum = 0;
+
+//         for (int j = 0; j < arr[i].size(); j++) {
+//             sum += arr[i][j];
+//         }
+
+//         ans.push_back(sum);
+//     }
+
+//     return ans;
+// }
