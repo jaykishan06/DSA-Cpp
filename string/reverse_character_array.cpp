a718c851f12ad4362c9fc5eac906a7c02c2e49f6
jaykishan06 <jaykishanvishwakarma06@gmail.com>
2026-09-05 11:14:35 +0530

    added logic for reversing the string

---
+++ string/reverse_character_array.cpp
@@ -0,0 +1,14 @@
+//   class Solution {
+// public:
+//     void reverseCharArray(vector<char>& arr) {
+//         int start = 0;
+//         int end = arr.size() - 1;
+
+//         while (start < end) {
+//             swap(arr[start], arr[end]);
+
+//             start++;
+//             end--;
+//         }
+//     }
+// };
